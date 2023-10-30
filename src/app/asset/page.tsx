8ca83f372a405f9461213887aee3e2ae3e1efeb7
komosyu4649 <komosyu4649@gmail.com>
2023-10-30 20:49:02 +0900

    assetページ追加

--- src/app/asset/page.tsx
+++ src/app/asset/page.tsx
@@ -5,16 +5,18 @@ import Button from '@/components/Button'
 import React from 'react'
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
 import { Pie } from 'react-chartjs-2'
+import ChartPie from '@/components/asset/ChartPie'
+import { urbanist } from '../fonts'
 
 ChartJS.register(ArcElement, Tooltip, Legend)
 
 const page = () => {
   const data = {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    labels: ['洋服', 'キャップ', 'スニーカー', '美容', '書籍', 'デバイス'],
     datasets: [
       {
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
+        label: '総額',
+        data: [68000, 19000, 30000, 5200, 2200, 3000],
         backgroundColor: [
           'rgba(255, 99, 132, 0.2)',
           'rgba(54, 162, 235, 0.2)',
@@ -55,13 +57,23 @@ const page = () => {
           今欲しいモノと所有しているものの総額。
           <br /> 好きなものにお金を使えているか確認しよう。
         </p>
-        <div className='flex flex-row gap-4 mt-8'>
+        <div className='flex flex-row gap-4 mt-8 mb-10'>
           <Button href='/asset/property'>所有しているモノ</Button>
           <Button href='/asset/want' color='light'>
             欲しいモノ
           </Button>
         </div>
-        <Pie data={data} options={{ responsive: true, maintainAspectRatio: true }} />
+        <div className='flex flex-col items-center gap-6 p-8 border border-line rounded-xl'>
+          <h2
+            className={`inline-block w-fit px-6 py-2 text-[1.2rem] font-bold bg-gray rounded-full ${urbanist.className}`}
+          >
+            Total : {''}¥
+            {data.datasets[0].data
+              .reduce((accumulator, currentValue) => accumulator + currentValue)
+              .toLocaleString()}
+          </h2>
+          <ChartPie data={data} options={{ responsive: true, maintainAspectRatio: true }} />
+        </div>
       </main>
     </>
   )
