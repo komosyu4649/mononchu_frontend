777314eb9606236693e4786f6679e4e717035e12
komosyu4649 <komosyu4649@gmail.com>
2023-11-28 09:31:54 +0900

    Add MemoContainer component to page.tsx

--- src/app/stuff/[category]/property/[item]/page.tsx
+++ src/app/stuff/[category]/property/[item]/page.tsx
@@ -5,6 +5,7 @@ import Input from '@/components/Input'
 import Modal from '@/components/Modal'
 import ItemInformation from '@/components/stuff/ItemInformation'
 import ItemModals from '@/components/stuff/ItemModals'
+import MemoContainer from '@/components/stuff/MemoContainer'
 import { Memo, StuffProperty } from '@/type'
 import axios from 'axios'
 import Image from 'next/image'
@@ -118,82 +119,7 @@ export default async function StuffDetailProperty(props: Props) {
           itemDetailData={itemDetailData}
           itemInfoList={itemInfoList}
         /> */}
-        <div className=''>
-          <form action='' className=''>
-            <div className='p-6 bg-gray border border-line rounded-xl'>
-              <div className='grid grid-cols-[1fr_auto] justify-between items-center mb-4'>
-                <ul className='flex flex-row gap-2'>
-                  {fiveW.map((item, index) => (
-                    <li key={item.param} className=''>
-                      <Link
-                        href={{
-                          pathname: '/stuff/detail/property/detail',
-                          query: { five: item.param },
-                        }}
-                        className={`inline-block px-4 py-[.15rem] text-[1.2rem] border border-line rounded-full bg-white ${urbanist.className}`}
-                      >
-                        {item.text}
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-                <label
-                  htmlFor='dropzone-file'
-                  className='flex flex-col items-center justify-center'
-                >
-                  <div className='h-fit p-3 bg-black rounded-full'>
-                    <Image
-                      src='/assets/img/common/icon_picture.svg'
-                      alt='画像'
-                      width={100}
-                      height={100}
-                      className='w-6 h-6'
-                    />
-                  </div>
-                  <input id='dropzone-file' type='file' className='hidden' />
-                </label>
-              </div>
-              <textarea
-                name=''
-                id=''
-                cols={10}
-                rows={3}
-                placeholder='なんで気に入っているのか教えてちょうだい'
-                className='w-full mb-4 p-3 text-defaultText border border-line rounded-md'
-              ></textarea>
-              <Button>投稿する</Button>
-            </div>
-          </form>
-          <ul className='flex flex-col gap-3 mt-6'>
-            {memosData.map((memo: Memo, index: number) => {
-              return (
-                <li key={memo.id} className=''>
-                  <div className='p-6 border border-line rounded-xl'>
-                    <div className='grid grid-cols-[1fr_auto] justify-between items-center mb-4'>
-                      <ul className='flex flex-row gap-2'>
-                        {memo.fiveW.map((item, index) => (
-                          <li key={item} className=''>
-                            <span
-                              className={`inline-block px-4 py-[.15rem] text-[1.2rem] border border-line rounded-full bg-white ${urbanist.className}`}
-                            >
-                              {item}
-                            </span>
-                          </li>
-                        ))}
-                      </ul>
-                      <time className={`text-[1.2rem] ${urbanist.className}`}>2021/09/01</time>
-                    </div>
-                    <p className='text-defaultText'>{memo.memo}</p>
-                    <div className='flex flex-row gap-4 justify-end'>
-                      <button className='inline text-[1.2rem] underline'>編集</button>
-                      <button className='inline text-[1.2rem] underline'>削除</button>
-                    </div>
-                  </div>
-                </li>
-              )
-            })}
-          </ul>
-        </div>
+        <MemoContainer type='property' category={category} item={item} />
       </main>
     </>
   )
